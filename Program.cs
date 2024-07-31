581438fd940d60094a9c2a7311faebe0d46c3c3c
Syn Scorps <iasyasdnn@live.fr>
2024-07-31 15:08:57 +0200

    port

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace StockTracker.Client
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:32770/") });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:32770/") });
 
 
             builder.Services.AddScoped<IInventoryService, InventoryService>();
